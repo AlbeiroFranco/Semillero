7ad19be474d755e89751aa4f7425b481910a8326
Jose Albeiro <Jose Albeiro Franco>
2021-09-09 14:05:45 -0500

    Actualizacion ejercicios

---
+++ WorkOut/10 Ejercicio/script2.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('input');
+
+button.addEventListener('click', ()=>{
+    agregarinputs(agregarinputs)
+})
+    
+    
+function agregarinputs(){
+    let input1 = document.createElement('input')
+    input1.value = 'Frontend';
+    
+    document.getElementById('cardinput').append(input1);
+   
+
+}
+
+
