abb0fe257af4bac9b6b533214f46d98b1fbf3c9c
Jose Albeiro Franco <jose.franco@sinco.com.co>
2022-02-16 18:36:34 -0500

    AngularJS

---
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -0,0 +1,4 @@
+/* const nombre = prompt('cual es tu nombre');
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo ` */
+
+const producto = 'Monitor 24 Pulgadas';
---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,6 @@
+let producto = 'Monitor de 24pulgadas';
+console.log(producto);
+producto = 'Monitor de 19 Pulgadas';
+console.log(producto);
+producto = 20;
+console.log(producto);
